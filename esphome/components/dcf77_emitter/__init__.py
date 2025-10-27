ed4280f80c1e26def64e1d93179e55f64ce4f5c1
Benjamin Beichler <Hadrian2002@googlemail.com>
2025-10-27 12:55:04 +0100

    Integrate logging into dcf77_emitter component
    Added logging functionality to dcf77_emitter.

--- esphome/components/dcf77_emitter/__init__.py
+++ esphome/components/dcf77_emitter/__init__.py
@@ -4,6 +4,8 @@ from esphome import pins
 from esphome.components import time, switch
 from esphome.const import CONF_ID, CONF_TIME_ID
 
+import logging  # <- add this import
+
 DEPENDENCIES = ["time"]
 MULTI_CONF = True
 
@@ -22,18 +24,35 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_SYNC_SWITCH_ID): cv.use_id(switch.Switch),
 }).extend(cv.COMPONENT_SCHEMA)
 
+_LOGGER = logging.getLogger(__name__)  # <- logger for structured logs
+
 async def to_code(config):
+    # Simple stdout print (always visible)
+    print("dcf77_emitter.to_code: starting with config:", config)
+
+    # Structured logging (may be filtered depending on esphome logging)
+    _LOGGER.info("dcf77_emitter.to_code: creating Pvariable for id=%s", config.get("id"))
+
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
+    # After registration show confirmation
+    print("dcf77_emitter.to_code: registered component variable:", var)
+
     time_ = await cg.get_variable(config[CONF_TIME_ID])
     cg.add(var.set_time_id(time_))
+    print("dcf77_emitter.to_code: set_time_id done")
 
     pin = await cg.gpio_pin_expression(config[CONF_ANTENNA_PIN])
     cg.add(var.set_antenna_pin(pin))
+    print("dcf77_emitter.to_code: set_antenna_pin done ->", pin)
 
     pin = await cg.gpio_pin_expression(config[CONF_LED_PIN])
     cg.add(var.set_led_pin(pin))
+    print("dcf77_emitter.to_code: set_led_pin done ->", pin)
     
     switch_ = await cg.get_variable(config[CONF_SYNC_SWITCH_ID])
     cg.add(var.set_sync_switch(switch_))
+    print("dcf77_emitter.to_code: set_sync_switch done ->", switch_)
+
+    _LOGGER.info("dcf77_emitter.to_code: finished") 
